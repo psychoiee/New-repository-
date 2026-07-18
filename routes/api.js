ef9079c9a572b0bee9f0dedc71afc14b5d3dc06c
psychoiee <seven_wa007@yahoo.com>
2026-07-18 18:38:05 +0500

    Implement API routes for chains and transactions

---
+++ routes/api.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const store = require("../services/store");
+const { evmChains, rpcChains, nonEvmChains } = require("../config/chains");
+
+const router = express.Router();
+
+router.get("/chains", (req, res) => {
+  const all = [...evmChains, ...rpcChains, ...nonEvmChains].map((c) => ({ id: c.id, label: c.label, symbol: c.nativeSymbol }));
+  res.json(all);
+});
+
+router.get("/transactions", (req, res) => {
+  const chain = req.query.chain || "ALL";
+  const minUsd = req.query.minUsd ? Number(req.query.minUsd) : undefined;
+  res.json(store.getFeed({ chain, minUsd }));
+});
+
+module.exports = router;
