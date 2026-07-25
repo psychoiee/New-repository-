3c3536f897e8be5c1be7e9c2670494ccd940b0db
psychoiee <seven_wa007@yahoo.com>
2026-07-25 20:51:09 +0500

    Remove LAB token tracking, keep USDT only

--- config/tokens.js
+++ config/tokens.js
@@ -1,6 +1,5 @@
 module.exports = {
   bsc: [
-    { symbol: "LAB", address: "0x7ec43Cf65F1663F820427C62A5780b8f2E25593A", decimals: 18, coingeckoId: "lab" },
     { symbol: "USDT", address: "0x55d398326f99059fF775485246999027B3197955", decimals: 18, coingeckoId: "tether" },
   ],
   ethereum: [
