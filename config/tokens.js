d36218df5a408662097abe928e2baa57a7219cbc
psychoiee <seven_wa007@yahoo.com>
2026-07-25 20:47:16 +0500

    Track USDT on all EVM chains

--- config/tokens.js
+++ config/tokens.js
@@ -1,9 +1,18 @@
 module.exports = {
   bsc: [
     { symbol: "LAB", address: "0x7ec43Cf65F1663F820427C62A5780b8f2E25593A", decimals: 18, coingeckoId: "lab" },
+    { symbol: "USDT", address: "0x55d398326f99059fF775485246999027B3197955", decimals: 18, coingeckoId: "tether" },
   ],
-  ethereum: [],
-  polygon: [],
-  arbitrum: [],
-  base: [],
+  ethereum: [
+    { symbol: "USDT", address: "0xdAC17F958D2ee523a2206206994597C13D831ec7", decimals: 6, coingeckoId: "tether" },
+  ],
+  polygon: [
+    { symbol: "USDT", address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F", decimals: 6, coingeckoId: "tether" },
+  ],
+  arbitrum: [
+    { symbol: "USDT", address: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9", decimals: 6, coingeckoId: "tether" },
+  ],
+  base: [
+    { symbol: "USDT", address: "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2", decimals: 6, coingeckoId: "tether" },
+  ],
 };
